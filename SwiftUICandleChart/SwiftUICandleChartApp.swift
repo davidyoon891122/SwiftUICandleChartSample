69fed70b7fc025da0b4ee1ca2094c3819a49ddb5
David Yoon <davidyoon@hanwha.com>
2024-02-22 08:44:21 +0900

    Initial Commit

---
+++ SwiftUICandleChart/SwiftUICandleChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICandleChartApp.swift
+//  SwiftUICandleChart
+//
+//  Created by Davidyoon on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICandleChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
